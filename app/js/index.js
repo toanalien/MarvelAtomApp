0fbfc71ecc9427c21b8479ca40c5269cef711276
Toan Vo <toanalien@gmail.com>
2016-11-04 00:06:50 +0700

    add function load json

--- app/js/index.js
+++ app/js/index.js
@@ -8,3 +8,25 @@ var ts = String(new Date().getTime());
 var hash = md5(ts+privateKey+publicKey);
 
 var url = `https://gateway.marvel.com/v1/public/characters?ts=${ts}&apikey=${publicKey}&hash=${hash}&limit=25`;
+
+var characterHTML = (character)=> `
+  <div class="character">
+    <h2>${character.name}</h2>
+    <img src="${character.thumbnail.path}.${character.thumbnail.extension}" />
+  </div>`;
+
+fetch(url)
+    .then(resp => resp.json())
+    .then(json => json.data.results)
+    .then(characters => {
+        var html = characters.map(characterHTML).join('');
+        var characterList = document.getElementById("character_list");
+        characterList.innerHTML = html;
+
+        new Notification(document.title, {
+            body: 'Super Heroes Loaded!'
+        });
+    })
+    .catch((error)=> {
+        console.error(error);
+    });
